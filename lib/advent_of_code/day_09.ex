8de28d0a1d03abf8d62a170e38cabef5ffa2f436
Jackson McMath <jackson.mcmath@cfacorp.com>
2023-12-12 10:21:31 -0500

    Add day 09 solutions

--- lib/advent_of_code/day_09.ex
+++ lib/advent_of_code/day_09.ex
@@ -1,7 +1,42 @@
 defmodule AdventOfCode.Day09 do
-  def part1(_args) do
+  def part1(input) do
+    input
+    |> parse_input()
+    |> Enum.map(&extrapolate_last/1)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(input) do
+    input
+    |> parse_input()
+    |> Enum.map(&extrapolate_first/1)
+    |> Enum.sum()
+  end
+
+  defp parse_input(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      line
+      |> String.split(" ", trim: true)
+      |> Enum.map(&String.to_integer/1)
+    end)
+  end
+
+  defp extrapolate_last(values) when length(values) > 1 do
+    values |> differences() |> extrapolate_last() |> Kernel.+(List.last(values))
+  end
+
+  defp extrapolate_last([value]), do: value
+
+  defp extrapolate_first(values) when length(values) > 1 do
+    dif = values |> differences() |> extrapolate_first()
+    List.first(values) - dif
+  end
+
+  defp extrapolate_first([value]), do: value
+
+  defp differences(values) when is_list(values) do
+    Enum.zip(values, Enum.drop(values, 1)) |> Enum.map(fn {a, b} -> b - a end)
   end
 end
